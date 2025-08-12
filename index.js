9fd8c10138b518f5d40e2986b55aee7433814c09
likhithsantosh <156637680+likhithsantosh@users.noreply.github.com>
2025-08-12 16:58:29 +0530

    index.js

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ import connectDB from './config/db.js';
 // Inngest
 import { inngest, functions } from './inngest/Server.js';
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+
 
 
 // Routes
@@ -29,6 +27,10 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 // Routes
 app.use('/api/inngest', serve({ client: inngest, functions }));
