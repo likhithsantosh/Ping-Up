1a884d1bb1cc515f7c519d79b59e6cd146980bf6
likhithsantosh <156637680+likhithsantosh@users.noreply.github.com>
2025-08-12 16:50:57 +0530

    index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express';
-import pkg from 'inngest';
-const { serve } = pkg;
+import { serve } from 'inngest/express';
 
 import connectDB from './config/db.js';
 
